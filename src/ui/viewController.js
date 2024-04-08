222bdabea7dd091266c68f72e5047e57606dc314
r0astPiGGy <1forzyg1@gmail.com>
2024-04-08 16:23:39 +0300

    Update viewController.js

--- src/ui/viewController.js
+++ src/ui/viewController.js
@@ -1,13 +1,37 @@
 import {GetPosts} from "../domain/useCases/getPosts.js";
 
+const ViewState = (isLoading = true) => ({
+    isLoading: isLoading,
+    posts: [],
+    page: 0,
+    searchQuery: null
+})
+
 export class ViewController {
 
+    #viewStateUpdatedListener
     #getPosts
+
+    #state = ViewState()
 
     constructor(getPosts = new GetPosts()) {
         this.#getPosts = getPosts
     }
 
+    #fireViewStateUpdated = () => {
+        if (this.#viewStateUpdatedListener === null) return
 
+        this.#viewStateUpdatedListener(this.#state)
+    }
+
+    setViewStateUpdatedListener = (listener) => this.#viewStateUpdatedListener = listener
+
+    onQueryChanged = (searchQuery) => {
+
+    }
+
+    onLoadMore = () => {
+
+    }
 
 }
